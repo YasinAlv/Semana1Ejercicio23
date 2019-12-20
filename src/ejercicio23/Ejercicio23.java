fd5727fb7ec393ded8fc3e14e6045d5def8134f8
Ingenieria <Ingenieria@DESKTOP-17V176H>
2019-12-20 12:58:05 -0600

    Ejercicio #23

---
+++ src/ejercicio23/Ejercicio23.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio23;
+
+/**
+ *
+ * @author Ingenieria
+ */
+public class Ejercicio23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int contador = 0;
+        String frase = "La lluvia en Sevilla es una maravilla";
+        System.out.println(frase);
+        
+        frase = frase.replace('a', 'e');
+        System.out.println(frase);
+        
+    }
+
+}
